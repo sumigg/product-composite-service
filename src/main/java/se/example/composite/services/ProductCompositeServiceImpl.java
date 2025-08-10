c51aba81c1a6d8c765826c56819b3bd0675dde3f
sumigg <with.sumitaggarwal@gmail.com>
2025-08-10 12:37:17 +0530

    add initial code for product-composite-service

---
+++ src/main/java/se/example/composite/services/ProductCompositeServiceImpl.java
@@ -0,0 +1,84 @@
+package se.example.composite.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import se.example.api.composite.product.ProductAggregate;
+import se.example.api.composite.product.ProductCompositeService;
+import se.example.api.composite.product.RecommendationSummary;
+import se.example.api.composite.product.ReviewSummary;
+import se.example.api.composite.product.ServiceAddresses;
+import se.example.api.core.product.Product;
+import se.example.api.core.recommendation.Recommendation;
+import se.example.api.core.review.Review;
+import se.example.api.exception.NotFoundException;
+import se.example.util.http.ServiceUtil;
+
+@RestController
+public class ProductCompositeServiceImpl implements ProductCompositeService {
+
+    private final ServiceUtil serviceUtil;
+    private ProductCompositeIntegration integration;
+
+    @Autowired
+    public ProductCompositeServiceImpl(
+            ServiceUtil serviceUtil, ProductCompositeIntegration integration) {
+
+        this.serviceUtil = serviceUtil;
+        this.integration = integration;
+    }
+
+    @Override
+    public ProductAggregate getProduct(int productId) {
+
+        Product product = integration.getProduct(productId);
+        if (product == null) {
+            throw new NotFoundException("No product found for productId: " + productId);
+        }
+
+        List<Recommendation> recommendations = integration.getRecommendations(productId);
+
+        List<Review> reviews = integration.getReviews(productId);
+
+        return createProductAggregate(product, recommendations, reviews, serviceUtil.getServiceAddress());
+    }
+
+    private ProductAggregate createProductAggregate(
+            Product product,
+            List<Recommendation> recommendations,
+            List<Review> reviews,
+            String serviceAddress) {
+
+        // 1. Setup product info
+        int productId = product.getProductId();
+        String name = product.getName();
+        int weight = product.getWeight();
+
+        // 2. Copy summary recommendation info, if available
+        List<RecommendationSummary> recommendationSummaries = (recommendations == null) ? null
+                : recommendations.stream()
+                        .map(r -> new RecommendationSummary(r.getRecommendationId(), r.getAuthor(), r.getRate()))
+                        .collect(Collectors.toList());
+
+        // 3. Copy summary review info, if available
+        List<ReviewSummary> reviewSummaries = (reviews == null) ? null
+                : reviews.stream()
+                        .map(r -> new ReviewSummary(r.getReviewId(), r.getAuthor(), r.getSubject()))
+                        .collect(Collectors.toList());
+
+        // 4. Create info regarding the involved microservices addresses
+        String productAddress = product.getServiceAddress();
+        String reviewAddress = (reviews != null && reviews.size() > 0) ? reviews.get(0).getServiceAddress() : "";
+        String recommendationAddress = (recommendations != null && recommendations.size() > 0)
+                ? recommendations.get(0).getServiceAddress()
+                : "";
+        ServiceAddresses serviceAddresses = new ServiceAddresses(serviceAddress, productAddress, reviewAddress,
+                recommendationAddress);
+
+        return new ProductAggregate(productId, name, weight, recommendationSummaries, reviewSummaries,
+                serviceAddresses);
+    }
+}
---
+++ src/test/java/se/example/composite/CompositeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.example.composite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
